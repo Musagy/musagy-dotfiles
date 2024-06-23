dde31e7b4f7f5217b47d7dfe415454f626c81da1
Musagy <diegomusagy@gmail.com>
2024-06-23 00:58:40 -0500

    feat: :tada: MusagyDotfiles | v0..09

--- install_arch.sh
+++ install_arch.sh
@@ -3,7 +3,10 @@ CUSTOM_ZSH=true
 JAPANESE_FONTS=true
 KOREAN_FONTS=true
 AUR_MANAGER_DEFAULT=true
+WALLPAPER_FOLDER=~/Pictures/Wallpapers
+CUSTOM_ROFI=true
 BROWSER_DEFAULT=true
+CUSTOM_NVIM=true
 
 function presentation {
     echo -e " ______                                   _____              ___ _ _            "
@@ -18,7 +21,7 @@ function presentation {
 # > Instalando dependencias generales
 function installDependencies {
     sudo pacman -Syu
-    sudo pacman -S unzip rofi waybar --noconfirm
+    sudo pacman -S --needed curl git unzip rofi waybar jq --noconfirm
 }
 
 ## Configuracion de Fonts
@@ -95,7 +98,7 @@ function copyFastfetch {
 }
 
 function installZshCustom {
-    sudo pacman -S zsh fastfetch
+    sudo pacman -S --needed zsh fastfetch
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
     
     git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
@@ -107,6 +110,20 @@ function installZshCustom {
     copyZshConf
 
     copyFastfetch
+}
+
+function setWallpaperFolder {
+    # Variables
+    WAYPAPER_CONFIG="./config/waypaper/config.ini"
+    WALLPAPER_DEFAULT="$WALLPAPER_FOLDER/bg.png"
+
+    # Cambiando los parametros de la configuracion de waypaper
+    sed -i "s|^folder *=.*|folder = $WALLPAPER_FOLDER|" "$WAYPAPER_CONFIG"
+    sed -i "s|^wallpaper *=.*|wallpaper = $WALLPAPER_DEFAULT|" "$WAYPAPER_CONFIG"
+
+    # Creando y copiando la carpeta de Wallpapers donde debe de ir
+    mkdir -p "$WALLPAPER_FOLDER"
+    cp -r ./Wallpapers/. "$WALLPAPER_FOLDER"
 }
 
 # Función para instalar de AUR manager y dependencias
@@ -121,6 +138,7 @@ function installAurManager {
         # cd paru
         # makepkg -si
         # cd ..
+        # rm -rf paru
         echo "paru"
     else
         # Instalación de yay
@@ -129,6 +147,7 @@ function installAurManager {
         # cd yay
         # makepkg -si
         # cd ..
+        # rm -rf yay
         echo "yay"
     fi
 }
@@ -141,52 +160,7 @@ function installAurDependencies {
     fi
 }
 
-function installBrowser {
-    if [ "$BROWSER_DEFAULT" == true ]; then
-        if [ "$AUR_MANAGER_DEFAULT" == true ]; then
-            paru -S brave --noconfirm
-        else
-            yay -S brave --noconfirm
-        fi
-    else
-        sudo pacman -S firefox --noconfirm
-    fi
-}
-
-
-
-
-function copyFiles
-{
-    # Check if nvim directory exist
-    # if [ -d "$HOME/.config/nvim" ]; then
-    #     # Check if init.vim exist
-    #     echo -e "\n\tExiste el directorio nvim"
-    #     if [ -f "$HOME/.config/nvim/init.vim" ]; then
-    #         echo -e "\tExiste el archivo init.vim"
-    #         cp $HOME/.config/nvim/init.vim $HOME/.config/nvim/backup.vim
-    #         cp config/nvim/init.vim $HOME/.config/nvim/init.vim
-    #     else
-    #         echo -e "\n\tNo existe el archivo creando copiando ...\n"
-    #         cp config/nvim/init.vim $HOME/.config/nvim/
-    #     fi
-    # else
-    #     echo -e "\n\tEl directorio no existe copiando ...\n"
-	# mkdir $HOME/.config/nvim
-    #     cp config/nvim/init.vim $HOME/.config/
-    # fi
-
-    # Check if .zshrc exist
-    # if [ -f "$HOME/.zshrc" ]; then
-    #     echo -e "\n\tExiste el archivo .zshrc"
-    #     cp $HOME/.zshrc $HOME/.zshrc_backup
-    # else
-    #     echo -e "\n\tNo existe el archivo, copiando ...\n"
-    #     cp home/.zshrc $HOME/
-    # fi
-
-    # Copy hyprland files
-    # Check if hypr directory exist
+function copyHyprConf {
     if [ -d "$HOME/.config/hypr" ]; then
         echo -e "\n\tExiste el directorio hypr...\n\tCopiando archivos\n"
         cp config/hypr/* $HOME/.config/hypr
@@ -195,7 +169,9 @@ function copyFiles
         mkdir $HOME/.config/hypr
         cp config/hypr/* $HOME/.config/hypr
     fi
-    # Check if waybar directory exist
+}
+
+function copyWaybarConf {
     if [ -d "$HOME/.config/waybar" ]; then
         echo -e "\n\tExiste el directorio waybar...\n\tCopiando archivos\n"
         cp config/waybar/* $HOME/.config/waybar
@@ -204,46 +180,66 @@ function copyFiles
         mkdir $HOME/.config/waybar
         cp config/waybar/* $HOME/.config/waybar/
     fi
-    
-
 }
 
+function copyRofiConf {
+    if [ -d "$HOME/.config/rofi" ]; then
+        echo -e "\n\tExiste el directorio rofi...\n\tCopiando archivos\n"
+        cp -r config/rofi/* $HOME/.config/rofi
+    else
+        echo -e "\n\tEl directorio no existe...\n\tCreandolo...\n"
+        mkdir $HOME/.config/rofi
+        cp -r config/rofi/* $HOME/.config/rofi/
+    fi
+}
+function setRofiTheme {
+    git clone --depth=1 https://github.com/adi1090x/rofi.git
+    cd rofi
+    chmod +x setup.sh
+    ./setup.sh
+    cd ..
+    rm -rf rofi
+}
 
-function installDependencies1 
-{
-    echo -e "\n\tInstalando Dependencias"
-    sudo pacman -Syu
-    sudo pacman -S hyprland rofi waybar fastfetch gcc g++ git curl neovim kitty ranger thunar zsh eza
-    yay -S swww waypaper
-
-    # Install oh-my-zsh and plugins
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-    # Autosuggestions
-    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
-    # Syntax
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
-    # History
-    git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+function installBrowser {
+    if [ "$BROWSER_DEFAULT" == true ]; then
+        if [ "$AUR_MANAGER_DEFAULT" == true ]; then
+            paru -S brave --noconfirm
+        else
+            yay -S brave --noconfirm
+        fi
+    else
+        sudo pacman --needed -S firefox --noconfirm
+    fi
+}
+function setNeoVim {
+    sudo pacman -S --needed neovim
 
     sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
+    #Check if nvim directory exist
+    if [ -d "$HOME/.config/nvim" ]; then
+        # Check if init.vim exist
+        echo -e "\n\tExiste el directorio nvim"
+        if [ -f "$HOME/.config/nvim/init.vim" ]; then
+            echo -e "\tExiste el archivo init.vim"
+            cp $HOME/.config/nvim/init.vim $HOME/.config/nvim/backup.vim
+            cp config/nvim/init.vim $HOME/.config/nvim/init.vim
+        else
+            echo -e "\n\tNo existe el archivo creando copiando ...\n"
+            cp config/nvim/init.vim $HOME/.config/nvim/
+        fi
+    else
+        echo -e "\n\tEl directorio no existe copiando ...\n"
+	    mkdir $HOME/.config/nvim
+        cp config/nvim/init.vim $HOME/.config/
+    fi
 }
-
-#presentation
-
-#echo -e "\n\tCambiando la shell basica por zsh"
-
-#chsh -s $(which zsh)
-
-#copyFilesNvim
 
 function setCustomInstallation {
     # ZSH Custom
-    read -p "> Instalación y Customizacion ZSH (Y/n): " CUSTOM_ZSH_CHOISE
+    read -p "> Instalación y Customizacion de ZSH (Y/n): " CUSTOM_ZSH_CHOISE
     case $CUSTOM_ZSH_CHOISE in
         "Y" | "y" | "")
             CUSTOM_ZSH=true
@@ -303,6 +299,23 @@ function setCustomInstallation {
         esac
     done
 
+    # Carpeta de wallpapers
+    read -p "> Carpeta de wallpapers (por defecto: ~/Pictures/Wallpapers): " WALLPAPER_FOLDER
+    WALLPAPER_FOLDER=${WALLPAPER_FOLDER:-"~/Pictures/Wallpapers"}
+
+    # ROFI Custom
+    read -p "> Instalación y Customizacion de Rofi (Y/n): " CUSTOM_ROFI_CHOISE
+    case $CUSTOM_ROFI_CHOISE in
+        "Y" | "y" | "")
+            CUSTOM_ROFI=true
+            break
+            ;;
+        "N" | "n")
+            CUSTOM_ROFI=false
+            break
+            ;;
+    esac
+
     # Browser
     echo "> Selecciona el Navegador:"
     echo "    1) Brave (por defecto)"
@@ -325,6 +338,18 @@ function setCustomInstallation {
         esac
     done
 
+    # ROFI Custom
+    read -p "> Instalación y Customizacion de NeoVim. PS: no uso NeoVim (Y/n): " CUSTOM_NVIM_CHOISE
+    case $CUSTOM_NVIM_CHOISE in
+        "Y" | "y" | "")
+            CUSTOM_NVIM=true
+            break
+            ;;
+        "N" | "n")
+            CUSTOM_NVIM=false
+            break
+            ;;
+    esac
 }
 
 function installer {
@@ -339,15 +364,30 @@ function installer {
     echo -e ">>> Copiando configuracion de kitty"
     # copyKittyConf
     echo -e ">>> Instalando AUR manager"
-    installAurManager
+    # installAurManager
+    echo -e ">>> Configurando la carpeta de Wallpapers y pre montando Waypaper conf"
+    # setWallpaperFolder
     echo -e ">>> Instalando AUR dependencias"
-    installAurDependencies
-    echo -e ">>> Copiando configuracion de hyprland"
     # installAurDependencies
-
-
-    echo -e "Instalando Navegador"
+    echo -e ">>> Creando carpeta de Wallpaper"
+    # installAurDependencies
+    echo -e ">>> Copiando configuracion de hyprland"
+    # copyHyprConf
+    echo -e ">>> Copiando configuracion de waybar"
+    # copyWaybarConf
+    if [ "$CUSTOM_ROFI" == true ]; then
+        echo -e ">>> Instalando tema de rofi"
+        # setRofiTheme
+        echo -e ">>> Copiando configuracion de rofi"
+        # copyRofiConf
+    fi
+    echo -e ">>> Instalando Navegador"
     #installAurDependencies
+
+    if [ "$CUSTOM_ZSH" == true ]; then
+        echo ">>> Instalando y configurando NeoVim"
+        setNeoVim
+    fi
 }
 
 function setTypeInstall {
@@ -379,11 +419,3 @@ function setTypeInstall {
 
 presentation
 setTypeInstall
-
-# installDependencies
-# installAurMAnager 1
-
-# installFonts
-# copyKittyConf
-
-# installZshDependencies
